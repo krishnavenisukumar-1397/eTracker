26d84b7dddd50d6bcd52f930c57a1311d9e4a5f0
krishnavenisukumar-1397 <62061304+krishnavenisukumar-1397@users.noreply.github.com>
2020-04-02 11:30:39 +0530

    creating an Dto for expense

---
+++ ExpenseTrackerManagement/src/main/java/com/expense/Dto/ExpenseRequestDto.java
@@ -0,0 +1,53 @@
+package com.expense.Dto;
+
+import java.time.LocalDate;
+
+public class ExpenseRequestDto {
+
+	private Long expenseId;
+	private String type;
+	private String status;
+	private LocalDate date;
+	private Double totalAmount;
+
+	public Long getExpenseId() {
+		return expenseId;
+	}
+
+	public void setExpenseId(Long expenseId) {
+		this.expenseId = expenseId;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+
+	public Double getTotalAmount() {
+		return totalAmount;
+	}
+
+	public void setTotalAmount(Double totalAmount) {
+		this.totalAmount = totalAmount;
+	}
+
+}
---
+++ ExpenseTrackerManagement/src/main/java/com/expense/Dto/ExpenseResponseDto.java
@@ -0,0 +1,15 @@
+package com.expense.Dto;
+
+public class ExpenseResponseDto {
+
+	private String displayMessage;
+
+	public String getDisplayMessage() {
+		return displayMessage;
+	}
+
+	public void setDisplayMessage(String displayMessage) {
+		this.displayMessage = displayMessage;
+	}
+
+}
